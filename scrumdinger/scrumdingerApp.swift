70582b84476e48bea9f88808d0a92918c44244cd
Yevhenii Romanyuk <hotpick18@gmail.com>
2023-06-22 14:37:01 +0200

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Zhenya Romaniyk on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
